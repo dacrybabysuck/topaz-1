e4c62a24b18d06704d8c76160f9b5e66bb5136a0
jdsmall25 <github@foreversmall.com>
2021-04-06 16:20:51 -0400

    [update] Simurgh draw-in, 2hr, and stat mods

--- scripts/zones/Rolanberry_Fields/mobs/Simurgh.lua
+++ scripts/zones/Rolanberry_Fields/mobs/Simurgh.lua
@@ -2,10 +2,26 @@
 -- Area: Rolanberry Fields (110)
 --  HNM: Simurgh
 -----------------------------------
-mixins = {require("scripts/mixins/rage")}
+mixins =
+{
+    require("scripts/mixins/job_special"),
+    require("scripts/mixins/rage")
+}
 require("scripts/globals/titles")
+require("scripts/globals/status")
 -----------------------------------
 local entity = {}
+
+entity.onMobInitialize = function(mob)
+    mob:setMobMod(tpz.mobMod.DRAW_IN, 1)
+end
+
+entity.onMobSpawn = function(mob)
+    mob:addMod(tpz.mod.ACC, 50)
+    mob:addMod(tpz.mod.EVA, 50)
+    mob:addMod(tpz.mod.ATT, -75)
+    mob:addMod(tpz.mod.STATUSRES, -50)
+end
 
 entity.onMobDeath = function(mob, player, isKiller)
     player:addTitle(tpz.title.SIMURGH_POACHER)
